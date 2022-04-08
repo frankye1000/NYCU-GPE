e61e989475ef0274d50baf26fcbd3f520d2630b1
unknown <frankye1000>
2022-04-09 06:46:21 +0800

    update 20220409

---
+++ 11448.cpp
@@ -0,0 +1,18 @@
+/* a884: 11448 - Crisis */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,a,b;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        cout<<a-b<<endl;
+
+    }
+
+
+    return 0;
+}
---
+++ 11448.py
@@ -0,0 +1,7 @@
+''' a884: 11448 - Crisis '''
+
+a = '9314'
+b = '8996'
+
+for i in range(0,max(len(a),len(b)),-1):
+    print(i)
